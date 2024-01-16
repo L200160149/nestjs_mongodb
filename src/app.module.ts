2a6bf00f0934fab75519383cf75fe44152a5a15a
l200160149 <ums.informatika@gmail.com>
2024-01-16 08:55:21 +0700

    user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://l200160149:l200160149@cluster0.m7nma.mongodb.net/?retryWrites=true&w=majority', {dbName:'nestjs_mongo'}),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
